152e6c902693d97fe75a08d0523a2f982aea7239
Spartan-ash <ashishhsingh84@gmail.com>
2020-07-19 03:18:34 +0530

    Updates

---
+++ precache-manifest.bf13f4e253d83743e8d2b9681ac87278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a6ca50bb1fcf99c6254",
+    "url": "/my-pokedex/static/css/main.939665d1.chunk.css"
+  },
+  {
+    "revision": "3a6ca50bb1fcf99c6254",
+    "url": "/my-pokedex/static/js/main.3b289f53.chunk.js"
+  },
+  {
+    "revision": "ac6baddaabfdd14c7fca",
+    "url": "/my-pokedex/static/js/runtime~main.99d3d0d7.js"
+  },
+  {
+    "revision": "f909e4803f9e6a4524aa",
+    "url": "/my-pokedex/static/css/2.2522bb33.chunk.css"
+  },
+  {
+    "revision": "f909e4803f9e6a4524aa",
+    "url": "/my-pokedex/static/js/2.2b875f5c.chunk.js"
+  },
+  {
+    "revision": "92fe99d39bf43ff83bf0efacd2852a05",
+    "url": "/my-pokedex/index.html"
+  }
+];
